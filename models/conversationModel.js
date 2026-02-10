209ff268d378085febbad34d19cbc54efa419dca
Ekramy Ashraf <ekramyashraf@gmail.com>
2026-02-10 19:57:16 +0200

    feat(conversation):add conversation model #2

---
+++ models/conversationModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const conversationSchema = new mongoose.Schema(
+  {
+    members: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+module.exports = Conversation;
